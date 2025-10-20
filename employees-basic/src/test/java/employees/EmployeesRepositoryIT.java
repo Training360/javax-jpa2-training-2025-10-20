4df9ba8bc59f5c3d35e85e2f5bf6d3bd5c541a52
Istvan Viczian <viczian.istvan@gmail.com>
2025-10-20 12:58:39 +0200

    SELECT-ek számára assert Hibernate Statistics használatával

--- employees-basic/src/test/java/employees/EmployeesRepositoryIT.java
+++ employees-basic/src/test/java/employees/EmployeesRepositoryIT.java
@@ -1,5 +1,7 @@
 package employees;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -8,6 +10,7 @@ import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 //@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
@@ -17,14 +20,22 @@ class EmployeesRepositoryIT {
     @Autowired
     EmployeesRepository repository;
 
+    @Autowired
+    EntityManagerFactory entityManagerFactory;
+
     @Test
     @Commit
     void saveThenFindAll() {
+        var statistics = entityManagerFactory.unwrap(SessionFactory.class).getStatistics();
+
         repository.save(new Employee("John"));
+        statistics.clear();
         var employees = repository.findAll();
         assertThat(employees)
                 .extracting(Employee::getName)
                 .containsExactly("John");
+
+        assertEquals(1, statistics.getQueryExecutionCount());
     }
 
 }
