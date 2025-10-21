8f8def5d8005d0823ca12933ec6a51e3094acad3
Istvan Viczian <viczian.istvan@gmail.com>
2025-10-21 16:42:33 +0200

    flush

--- employees-dirty/src/main/java/employees/EmployeesRepository.java
+++ employees-dirty/src/main/java/employees/EmployeesRepository.java
@@ -1,7 +1,16 @@
 package employees;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeesRepository extends JpaRepository<Employee, Long> {
+
+    @Query("select e from Employee e")
+    <T> List<T> findAllWithType(Class<T> type);
+
+    @Query(value = "select id, emp_name as name from employees", nativeQuery = true)
+    List<EmployeeResource> findAllWithNativeQuery();
 
 }
--- employees-dirty/src/test/java/employees/EmployeesRepositoryIT.java
+++ employees-dirty/src/test/java/employees/EmployeesRepositoryIT.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,6 +29,9 @@ class EmployeesRepositoryIT {
 
     @Autowired
     TransactionTemplate transactionTemplate;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Test
     @Commit
@@ -51,6 +57,36 @@ class EmployeesRepositoryIT {
                 repository.save(employee);
                 System.out.println(employee.getId());
             }
+        });
+    }
+
+    @Test
+    @Commit
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    void saveAndSelect() {
+        transactionTemplate.executeWithoutResult(status -> {
+            // PC open
+            var employee = new Employee("John");
+            System.out.println("Save employee");
+            repository.save(employee);
+            System.out.println("Update employee");
+            employee.setName("Jack");
+            System.out.println("Find all employees");
+            // flush: insert, update
+//            var employees = repository.findAllWithType(EmployeeResource.class);
+//            var employees = repository.findAllWithNativeQuery();
+
+            repository.flush();
+            var employees = jdbcTemplate.query("select id, emp_name as name from employees",
+                    (rs, rowNum) -> new EmployeeResource(
+                            rs.getLong("id"),
+                            rs.getString("name")
+                    ));
+
+            for (var e : employees) {
+                System.out.println(e.name());
+            }
+            // PC closed
         });
     }
 
